841dc835ea83e6791577f5561d9f6fd3269cb904
Nzola Kiampava <nzolakiampava@gmail.com>
2026-01-19 14:58:14 +0100

    Update: atualizacao da api do backend, em producao

--- src/lib/apolloClient.ts
+++ src/lib/apolloClient.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
 
 const httpLink = createHttpLink({
-  uri: 'http://localhost:4000/graphql',
+  uri: 'https://lideresangola-backend.vercel.app/graphql',
 });
 
 const client = new ApolloClient({
